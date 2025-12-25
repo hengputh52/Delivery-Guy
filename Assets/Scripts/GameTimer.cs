f470109f377b74f8c2850798c521c4acfd851425
hengputh52 <harrystyles10203040@gmail.com>
2025-12-25 11:26:05 +0700

    update readme documentation

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -33,8 +33,8 @@ public class GameTimer : MonoBehaviour
 
         if(currentTime <= 10f && anim != null)
         {
-            anim.SetTrigger("LowTime");
-            //anim.SetTrigger("LowerTime");
+            //anim.SetTrigger("LowTime");
+            anim.SetTrigger("LowerTime");
         }
         if (currentTime <= 0)
         {
